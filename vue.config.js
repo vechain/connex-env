d7e98f49197ec5840200b927e0818e357961316e
qianbin <cola.tin.com@gmail.com>
2019-02-19 16:21:51 +0800

    reset base url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     lintOnSave: false,
-    publicPath: process.env.NODE_ENV === 'production' ? '/connex-env/' : '/',
+    publicPath: '/',
     pwa: {
         workboxOptions: {
             skipWaiting: true,
