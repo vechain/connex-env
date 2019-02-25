78f15e782ae07ee560c305eae4f77281e4c03c8e
qianbin <cola.tin.com@gmail.com>
2019-02-25 22:40:35 +0800

    revert prerender

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path')
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
-
 module.exports = {
     lintOnSave: false,
     publicPath: '/',
@@ -9,13 +6,5 @@ module.exports = {
             skipWaiting: true,
             clientsClaim: true
         }
-    },
-    configureWebpack: {
-        plugins: [
-            new PrerenderSpaPlugin({
-                staticDir: path.resolve(__dirname, './dist'),
-                routes: ['/wallets']
-            })
-        ]
     }
 }
