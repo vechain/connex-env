f00af15466fb3418a29ed56a67aecc1745a66345
libotony <libotony@users.noreply.github.com>
2022-07-25 07:40:55 +0000

    deploy: e5a381a6eab284324775404ad95e69e7404126a5

---
+++ precache-manifest.5cef6dfa3ee021657c6f06e2ae674f4b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a834d15fed1902ed17b507842e2ddbc",
+    "url": "/img/thor-wallet.9a834d15.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7717b9139f5af16f77d90dcd77d27be1",
+    "url": "/img/mac-logo.7717b913.svg"
+  },
+  {
+    "revision": "a39252a60e2fbd629ae5",
+    "url": "/css/chunk-vendors.0e4e0d66.css"
+  },
+  {
+    "revision": "62fd4dec8fe8028a9ad1a4166979eb4e",
+    "url": "/img/sync-ss-dark.62fd4dec.png"
+  },
+  {
+    "revision": "2ea8334d183def29be7394296864c871",
+    "url": "/img/linux-logo.2ea8334d.svg"
+  },
+  {
+    "revision": "eff8b17aeeb5d897879937ed53567223",
+    "url": "/img/github-logo.eff8b17a.svg"
+  },
+  {
+    "revision": "032b9c0a5c4184e7ada83092c62cc16e",
+    "url": "/img/thorwallet-ss.032b9c0a.png"
+  },
+  {
+    "revision": "b6846d4e56c6f988fb71",
+    "url": "/js/app.5f31f492.js"
+  },
+  {
+    "revision": "a39252a60e2fbd629ae5",
+    "url": "/js/chunk-vendors.b70c0407.js"
+  },
+  {
+    "revision": "ab41a8c5695d131b56af405890bee089",
+    "url": "/img/sync-ss-light.ab41a8c5.png"
+  },
+  {
+    "revision": "42eac3a056db227ff2f1eef9946fcf34",
+    "url": "/img/sync-logo.42eac3a0.png"
+  },
+  {
+    "revision": "1623a961f0361ec0237900870d0593b6",
+    "url": "/img/windows-logo.1623a961.svg"
+  },
+  {
+    "revision": "b9c1b71b78284296179b6cf0ada9eb4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6846d4e56c6f988fb71",
+    "url": "/css/app.df9084c3.css"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/blocklist.txt"
+  },
+  {
+    "revision": "a295797fcc2a76117e67d8a336d9774e",
+    "url": "/404.html"
+  }
+];
